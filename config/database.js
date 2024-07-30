2e3d81aa36b553d39b23c4e9e96111dfb667e941
Reem Ali <afeem1202@gmail.com>
2024-07-30 19:40:40 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
 module.exports = {
-    database : 'mongodb://127.0.0.1:27017/Cakestore'
+  // database : 'mongodb://127.0.0.1:27017/Cakestore'
+  database:'mongodb+srv://afeem1202:qwerty123@cluster0.figuev1.mongodb.net/Cakestore'
+ 
    
 }
--- models/addressModel.js
+++ models/addressModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const addressSchema = new mongoose.Schema({
    userId : {type:mongoose.Schema.Types.ObjectId,
              ref : 'User'},
